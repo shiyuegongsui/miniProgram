722e9cf096de2778fc33f83ba412326b05f5ac25
shiyuegongsui <lyq378175470@qq.com>
2019-11-25 16:43:39 +0800

    增加dialog

--- app.js
+++ app.js
@@ -4,9 +4,12 @@
 import require from "./utils/require.js";
 wx.$ajax = require.ajax;
 
+import Dialog from './components/vant-weapp/dialog/dialog';
+wx.$Dialog = Dialog;
 
 App({
   onLaunch: function () {
+
   },
   globalData: {
     userInfo: null
--- pages/index/index.js
+++ pages/index/index.js
@@ -15,14 +15,23 @@ Page({
     hi: 'MINA'
   },
   //事件处理函数
-  bindViewTap: function () {
-  },
+  bindViewTap: function () { },
   onLoad: function () {
     console.log(this.customData.hi);
 
     this.customData.hi = 2
 
     console.log(this.customData.hi);
+
+    wx.$Dialog.confirm({
+      title: '标题',
+      message: '弹窗内容'
+    }).then(() => {
+      // on confirm
+    }).catch(() => {
+      // on cancel
+    });
+
     // wx.$ajax({
     //   url: "",
     //   type: "get",
@@ -30,18 +39,18 @@ Page({
     //   success: function () { }
     // })
   },
-  getUserInfo: function (e) {
-  },
+  getUserInfo: function (e) { },
 
   showPop() {
 
-    this.setData({
+    var myShow = this.selectComponent('#myShow');
+    //访问属性,使用data访问内部属性和组件属性
+
+    myShow.setData({
       show: true
     })
+    console.info(myShow);
+
   },
-  cancel() {
-    this.setData({
-      show: false
-    })
-  }
+
 })
